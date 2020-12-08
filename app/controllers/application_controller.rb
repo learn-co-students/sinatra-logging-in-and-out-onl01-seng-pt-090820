ea873bacf1f6358490965807c14f04b9fe1b56d1
DavidRMorphew <morphewd@umich.edu>
2020-12-08 17:57:58 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(name: params[:name], password: params[:password])
+    @user = User.find_by(username: params[:username], password: params[:password])
     if @user
       session[:user_id] = @user.id
       redirect "/account"
@@ -23,6 +23,7 @@ class ApplicationController < Sinatra::Base
     erb :error
   end
   get '/account' do
+    @user = User.find_by(id: session[:user_id])
     erb :account
   end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,6 +4,6 @@ class Helpers
     end
 
     def self.is_logged_in?(session)
-        @user ? true : false
+        (@user = User.find_by(id: session[:user_id])) ? true : false
     end
 end

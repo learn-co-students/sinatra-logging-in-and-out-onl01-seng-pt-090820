d91cb8b424f0c8b8926c8826ddf0f6d69452bd96
DavidRMorphew <morphewd@umich.edu>
2020-12-08 23:25:38 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
+    @user = User.find_by(username: params[:username])
     if @user
       session[:user_id] = @user.id
       redirect to "/account"
     end
-    redirect to "/error"
+    redirect to '/error'
   end
 
   get '/error' do

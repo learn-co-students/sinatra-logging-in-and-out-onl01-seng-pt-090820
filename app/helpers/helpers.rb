5cab58e521e8f3f2db387042ede9e5b453064d64
Mikelanne <mikelanne.wood@gmail.com>
2020-12-12 13:14:31 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+
+    def self.current_user(session={})
+        if @current_user
+            @current_user
+        else 
+            @current_user = User.find_by(id: session[:user_id])
+        end
+    end 
+
+    def self.is_logged_in?(session={})
+        !!session[:user_id]
+    end 
+
 end

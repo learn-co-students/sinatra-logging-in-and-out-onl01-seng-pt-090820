a37e2b014f64b8d023a29b8a97f24bc21eef0da8
Carlj0666 <carlj1881@yahoo.com>
2020-12-15 21:20:10 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @current_user = User.find_by_id(session[:user_id])
-    if @current_user
+    @user = User.find(session[:user_id])
+    if @user
       erb :account
     else
       erb :error
@@ -30,6 +30,7 @@ class ApplicationController < Sinatra::Base
 
   get "/logout" do
     session.clear
+    redirect to '/'
   end
 
 
